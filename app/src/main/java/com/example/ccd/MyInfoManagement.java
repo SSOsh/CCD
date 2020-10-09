ee284752465121ba6748d27a8678ca20e05a071a
HjuH <hjuhee9807@naver.com>
2020-10-09 15:30:14 +0900

    myInfo

---
+++ app/src/main/java/com/example/ccd/MyInfoManagement.java
@@ -0,0 +1,14 @@
+package com.example.ccd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyInfoManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_info_management);
+    }
+}
