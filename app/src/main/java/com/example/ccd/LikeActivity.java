725420482a0cc44a0626f94ab0b0c3ce7d2a847e
SSOsh <53353714+SSOsh@users.noreply.github.com>
2020-10-12 21:15:03 +0900

    20201012
    commit

---
+++ app/src/main/java/com/example/ccd/LikeActivity.java
@@ -0,0 +1,19 @@
+package com.example.ccd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.HttpURLConnection;
+
+public class LikeActivity extends AppCompatActivity {
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.like_main);
+
+    }
+}
--- app/src/main/java/com/example/ccd/MainActivity.java
+++ app/src/main/java/com/example/ccd/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.ccd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.HttpURLConnection;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //이 뒤의 내용은 연습용, 나중에 삭제
+        button2 = findViewById(R.id.button2);
+        final RequestHttpConnection httpConnection = new RequestHttpConnection();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                httpConnection.request("", null);
+            }
+
+        });
+
+
     }
 }
