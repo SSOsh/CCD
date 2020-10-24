bdaf00ec4142cf587bdfb026e1e792b08067da5e
SSOsh <53353714+SSOsh@users.noreply.github.com>
2020-10-25 01:10:27 +0900

    20201025
    네트워크 작업중

--- app/src/main/java/com/example/ccd/LikeActivity.java
+++ app/src/main/java/com/example/ccd/LikeActivity.java
@@ -13,7 +13,37 @@ public class LikeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.like_main);
+        setContentView(R.layout.activity_like);
 
     }
+
+    //  이 친구 recyclerview설정하고 각 좋아요마다 이미지 변경 예시임
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        final ToggleButton tb2 =
+//                (ToggleButton) this.findViewById(R.id.toggleButton2);
+//
+//        tb2.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                if(tb2.isChecked()){
+//                    tb2.setBackgroundDrawable(
+//                            getResources().
+//                                    getDrawable(R.drawable.angry_birds_yellow_button_by_vyndo_d3hc3yl)
+//                    );
+//                }else{
+//                    tb2.setBackgroundDrawable(
+//                            getResources().
+//                                    getDrawable(R.drawable.angry_birds_red_button_by_vyndo_d3hc3t4)
+//                    );
+//                } // end if
+//            } // end onClick()
+//        });
+//
+//
+//        출처: https://bitsoul.tistory.com/37 [Happy Programmer~]
 }
--- app/src/main/java/com/example/ccd/MainActivity.java
+++ app/src/main/java/com/example/ccd/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,13 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView home = findViewById(R.id.home);
 
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "출력할 문자열", Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
